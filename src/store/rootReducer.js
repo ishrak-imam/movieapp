d1f259a7875338390689da782e5df2e8c306a6c8
Ishrak <ishrak.imam@gmail.com>
2018-04-09 11:04:02 +0600

    clean-up state on logout

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,24 @@
+
 import { combineReducers } from 'redux';
+
+import {
+  LOGOUT_SUCS
+} from '../modules/auth/action';
 
 import * as authReducers from '../modules/auth/reducer';
 import { reducer as formReducer } from 'redux-form';
 
-export default combineReducers({
-  ...authReducers,
-  form: formReducer
+const appReducer = combineReducers({
+  form: formReducer,
+  ...authReducers
 });
+
+const rootReduces = (state, action) => {
+  // clean-up state on logout
+  if (action.type === LOGOUT_SUCS) {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
+export default rootReduces;
