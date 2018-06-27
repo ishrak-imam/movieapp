c74c81f27333e7a5a844255f6c623f03a87b1721
Ishrak <ishrak@cefalo.com>
2018-06-27 10:28:09 +0600

    logger style changed

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -9,8 +9,10 @@ const reduxFormActions = (actionName) => {
 const logger = store => next => action => {
   const result = next(action);
   if (!reduxFormActions(action.type)) {
-    console.log('%c DISPATCHING      :: ', 'color: green', action);
-    console.log('%c NEXT STATE       :: ', 'color: green', store.getState().toJS());
+    console.groupCollapsed('%c action', 'color: grey  ', action.type)
+    console.log('%c DISPATCH  :: ', 'color: green', action);
+    console.log('%c NXTSTATE  :: ', 'color: green', store.getState().toJS());
+    console.groupEnd()
   }
   return result;
 };
